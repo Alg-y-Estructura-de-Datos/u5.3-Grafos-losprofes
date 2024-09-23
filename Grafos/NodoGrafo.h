c8bdf412d69d48312c2b297c9337df38f3f44834
Matias Tosetti Docente <141670493+matiastosettiOficial@users.noreply.github.com>
2024-09-22 23:48:11 -0300

    Update NodoGrafo.h Se agrega aristas ponderadas

--- Grafos/NodoGrafo.h
+++ Grafos/NodoGrafo.h
@@ -4,14 +4,25 @@
 #include <iostream>
 #include <list>
 
-template <class T>
+/*template <class T>
 class NodoGrafo {
 public:
     T data;
     std::list<NodoGrafo<T>*> vecinos;
 
     NodoGrafo(const T& d) : data(d) {}
+};*/
+
+
+template <class T>
+class NodoGrafo {
+public:
+    T data;
+    std::list<std::pair<NodoGrafo<T>*, int>> vecinos; // Cada vecino incluye un peso
+
+    NodoGrafo(const T& d) : data(d) {}
 };
+
 
 #endif // NODOGRAFO_H
 
