96390efb140c7dabeff946c57a36347b9aa9fc3f
Matias Tosetti Docente <141670493+matiastosettiOficial@users.noreply.github.com>
2024-09-22 23:55:54 -0300

    Update Grafo.h se agregan métodos
    Se agregan métodos a la clase grafos:
    *eliminar arista entre vertices
    *corroborar relación entre vértices devolviendo bool
    *buscar nodos adyascentes
    *aristas ponderadas
    
    no está funcional para uso de grafo ponderado

--- Grafos/Grafo.h
+++ Grafos/Grafo.h
@@ -5,7 +5,7 @@
 #include <map>
 #include "NodoGrafo.h"
 
-template <class T>
+/*template <class T>
 class Grafo {
 private:
     std::map<T, NodoGrafo<T>*> nodos;
@@ -21,6 +21,29 @@ public:
 
 private:
     void eliminarAristas(NodoGrafo<T>* nodo);
+};*/
+
+template <class T>
+class Grafo {
+private:
+    std::map<T, NodoGrafo<T>*> nodos;
+
+public:
+    Grafo();
+    ~Grafo();
+    void agregarNodo(const T& nodo);
+    void agregarArista(const T& origen, const T& destino);
+    void agregarAristaConPeso(const T& origen, const T& destino, int peso);
+    void eliminarNodo(const T& nodo);
+    void eliminarArista(const T& origen, const T& destino);
+    bool estanConectados(const T& origen, const T& destino);
+    std::list<T> obtenerVecinos(const T& nodo);
+    void imprimir();
+
+private:
+    void eliminarAristas(NodoGrafo<T>* nodo);
+    bool dfs(const T& origen, const T& destino, std::map<T, bool>& visitado);
+
 };
 
 template <class T>
@@ -77,6 +100,71 @@ void Grafo<T>::eliminarNodo(const T& nodo) {
     delete nodos[nodo];
     nodos.erase(nodo);
 }
+
+
+template <class T>
+void Grafo<T>::eliminarArista(const T& origen, const T& destino) {
+    if (nodos.find(origen) == nodos.end() || nodos.find(destino) == nodos.end())
+        return;
+
+    // Eliminar arista en ambos nodos (grafo no dirigido)
+    nodos[origen]->vecinos.remove_if([destino](const std::pair<NodoGrafo<T>*, int>& vecino) {
+        return vecino.first->data == destino;
+    });
+    nodos[destino]->vecinos.remove_if([origen](const std::pair<NodoGrafo<T>*, int>& vecino) {
+        return vecino.first->data == origen;
+    });
+}
+
+template <class T>
+std::list<T> Grafo<T>::obtenerVecinos(const T& nodo) {
+    std::list<T> listaVecinos;
+
+    if (nodos.find(nodo) != nodos.end()) {
+        for (const auto& vecino : nodos[nodo]->vecinos) {
+            listaVecinos.push_back(vecino.first->data); // Guardamos los datos de los vecinos
+        }
+    }
+
+    return listaVecinos;
+}
+
+
+template <class T>
+void Grafo<T>::agregarAristaConPeso(const T& origen, const T& destino, int peso) {
+    if (nodos.find(origen) == nodos.end() || nodos.find(destino) == nodos.end())
+        return; // Si alguno de los nodos no existe
+
+    // Agregar el vecino con el peso a ambos nodos (grafo no dirigido)
+    nodos[origen]->vecinos.push_back({nodos[destino], peso});
+    nodos[destino]->vecinos.push_back({nodos[origen], peso});
+}
+
+
+template <class T>
+bool Grafo<T>::estanConectados(const T& origen, const T& destino) {
+    if (nodos.find(origen) == nodos.end() || nodos.find(destino) == nodos.end())
+        return false; // Si alguno de los nodos no existe en el grafo
+
+    std::map<T, bool> visitado; // Para marcar los nodos visitados
+    return dfs(origen, destino, visitado);
+}
+
+template <class T>
+bool Grafo<T>::dfs(const T& origen, const T& destino, std::map<T, bool>& visitado) {
+    if (origen == destino) return true; // Si encontramos el destino
+
+    visitado[origen] = true; // Marcamos el nodo como visitado
+
+    // Recorremos los vecinos del nodo
+    for (NodoGrafo<T>* vecino : nodos[origen]->vecinos) {
+        if (!visitado[vecino->data] && dfs(vecino->data, destino, visitado))
+            return true;
+    }
+    return false;
+}
+
+
 
 template <class T>
 void Grafo<T>::imprimir() {
